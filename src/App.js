10225d5bce03fad8217433772929da70df508870
Your Name <94886034+saumyaakumarasinghe@users.noreply.github.com>
2022-11-28 21:32:57 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=7db2bc9c';
+
+//Movie onject
+const movie1 = {
+    "Title": "Spiderman",
+    "Year": "2010",
+    "imdbID": "tt1785572",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
+//Main functional component
+const App = () => {
+
+    //Create a new function to fetch movies
+    const searchMovies = async(title) => {
+        //Call API
+        const response = await fetch(`${API_URL}&s=${title}`);
+        //Get data from the API
+        const data = await response.json();
+
+        console.log(data.Search);
+    
+    }
+
+    //Call only at the start
+    useEffect (() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
